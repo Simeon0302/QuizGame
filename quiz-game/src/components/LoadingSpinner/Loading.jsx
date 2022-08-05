8ce43dbf2565f0567057d4a5fc761fc0e17b0f1f
Simeon_32 <simeonvutov2005@gmail.com>
2022-08-05 09:48:21 +0300

    Made some style changes to the Loading spinner component

--- quiz-game/src/components/LoadingSpinner/Loading.jsx
+++ quiz-game/src/components/LoadingSpinner/Loading.jsx
@@ -4,7 +4,7 @@ import "./spinner.css";
 function Loading({message}) {
     return (
         <div className="spinner-container">
-            <h1>{message}</h1>
+            <h1 className="loadingMessage">{message}</h1>
             <div className="loading-spinner"> </div>
         </div>
     );
